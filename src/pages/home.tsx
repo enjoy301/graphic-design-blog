687bc2d873df91239183029d33229c6d307bc5f3
enjoy301 <eunjong147@pusan.ac.kr>
2022-12-21 01:14:35 +0900

    :star: new: home layout

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,25 @@
 import React from "react";
+import "./home.css";
+import profile_image from "../images/profile.png";
 
 export default function Home() {
   return (
-    <>
-      <h1>home</h1>
-    </>
+    <div className="container">
+      <div className="header">Seobin Yoon</div>
+      <div className="wrapper">
+        <div className="sidebar">
+          <div className="profile">
+            <img
+              src={profile_image}
+              alt="profile"
+              style={{ width: "100%", height: "100%" }}
+            />
+          </div>
+          <div className="menu"></div>
+          <div className="logo">Logo</div>
+        </div>
+        <div className="content"></div>
+      </div>
+    </div>
   );
 }
