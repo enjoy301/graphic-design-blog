724162ed7ea541e3591f56a7b082a879a02577b5
enjoy301 <eunjong147@pusan.ac.kr>
2022-12-20 17:39:12 +0900

    :star: new: react router setting

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+import Home from "./home";
+import Error from "./error";
+import About from "./about";
+import Contact from "./contact";
+import Post from "./post";
+
+export { Home, Error, About, Contact, Post };
