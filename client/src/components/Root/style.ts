67ebbff90a15e9789ac896d2b58317a55289dc7c
enjoy301 <eunjong147@pusan.ac.kr>
2022-12-22 12:21:25 +0900

    :shirt: refactor: root page S-dot style 적용

---
+++ client/src/components/Root/style.ts
@@ -0,0 +1,101 @@
+import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 85vh;
+  width: 70vw;
+  min-width: 250px;
+  border: 5px solid black;
+  box-sizing: border-box;
+  margin: auto;
+`;
+
+export const Header = styled.div`
+  padding: 0 20px;
+  height: 10%;
+  font-weight: bold;
+  font-size: 36px;
+  display: flex;
+  align-items: center;
+`;
+
+export const Body = styled.div`
+  display: flex;
+  height: 90%;
+  width: 100%;
+`;
+
+export const SideBar = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Content = styled.div`
+  aspect-ratio: 1;
+  border-top: 2px solid black;
+`;
+
+export const HeaderText = styled.p``;
+
+export const HeaderLink = styled(NavLink)`
+  display: block;
+  marginleft: auto;
+  width: auto;
+  height: 60%;
+`;
+
+export const HeaderIcon = styled.img`
+  width: auto;
+  height: 100%;
+`;
+
+export const ProfileBox = styled.div`
+  aspect-ratio: 1;
+  border-width: 3px 3px 3px 0;
+  border-color: black;
+  border-style: solid;
+  display: flex;
+`;
+
+export const ProfileImage = styled.img`
+  width: 80%;
+  height: 80%;
+  margin: auto;
+`;
+
+export const MenuBox = styled.div`
+  flex: 1;
+  border-right: 1px solid black;
+  font-size: 20px;
+`;
+
+export const MenuLink = styled(NavLink)`
+  display: block;
+  text-decoration: none;
+  color: black;
+  &.active {
+    background-color: rgba(242, 228, 191, 0.79);
+  }
+`;
+
+export const MenuText = styled.p``;
+
+export const LogoBox = styled.div`
+  height: 10%;
+  border-top: 1px solid black;
+  border-right: 1px solid black;
+  display: flex;
+  text-align: center;
+  align-items: center;
+`;
+
+export const Logo = styled.p`
+  margin: auto;
+  padding: 5% 10%;
+  width: 50%;
+  font-size: 20px;
+  font-weight: bold;
+  border: 1px solid black;
+  border-radius: 100%;
+`;
--- client/src/pages/root.tsx
+++ client/src/pages/root.tsx
@@ -1,52 +1,44 @@
-import "./root.css";
 import profile_image from "../images/profile.png";
 import home_icon from "../images/home-icon.png";
-import MenuItem from "../components/menu-item";
-import { NavLink, Outlet } from "react-router-dom";
+import { Outlet } from "react-router-dom";
+import * as S from "../components/Root/style";
 
 export default function Home() {
   return (
-    <div className="container">
-      <div className="header">
-        <p>Seobin Yoon</p>
-        <NavLink
-          to="/"
-          style={{
-            display: "block",
-            marginLeft: "auto",
-            width: "auto",
-            height: "60%",
-          }}
-          className={({ isActive }) => (isActive ? "active" : "")}
-        >
-          <img
-            src={home_icon}
-            alt="home"
-            style={{ width: "auto", height: "100%" }}
-          />
-        </NavLink>
-      </div>
-      <div className="wrapper">
-        <div className="sidebar">
-          <div className="profile">
-            <img
-              src={profile_image}
-              alt="profile"
-              style={{ width: "80%", height: "80%", margin: "auto" }}
-            />
-          </div>
-          <div className="menu">
-            <MenuItem to="/about" content="It's me" />
-            <MenuItem to="/contact" content="Contact" />
-          </div>
-          <div className="logo">
-            <p className="logo__p">LOGO</p>
-          </div>
-        </div>
-        <div className="content">
+    <S.Container>
+      <S.Header>
+        <S.HeaderText>Seobin Yoon</S.HeaderText>
+        <S.HeaderLink to="/">
+          <S.HeaderIcon src={home_icon} alt="home" />
+        </S.HeaderLink>
+      </S.Header>
+      <S.Body>
+        <S.SideBar>
+          <S.ProfileBox>
+            <S.ProfileImage src={profile_image} alt="profile" />
+          </S.ProfileBox>
+          <S.MenuBox>
+            <S.MenuLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              <S.MenuText>It's me</S.MenuText>
+            </S.MenuLink>
+            <S.MenuLink
+              to="/contact"
+              className={({ isActive }) => (isActive ? "active" : "")}
+            >
+              <S.MenuText>Contact</S.MenuText>
+            </S.MenuLink>
+          </S.MenuBox>
+          <S.LogoBox>
+            <S.Logo>LOGO</S.Logo>
+          </S.LogoBox>
+        </S.SideBar>
+        <S.Content>
           <Outlet />
-        </div>
-      </div>
-    </div>
+        </S.Content>
+      </S.Body>
+    </S.Container>
   );
 }
